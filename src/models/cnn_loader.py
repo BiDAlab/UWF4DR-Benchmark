a0bbecdb0d3c74e04761ba05101859d06ab7d2f2
PabloJimenez823 <pablojimenezlizcano@gmail.com>
2026-02-05 18:29:21 +0100

    Update cnn_loader.py

--- src/models/cnn_loader.py
+++ src/models/cnn_loader.py
@@ -1,14 +1,17 @@
 from src.models.mobilenetv2 import load_mobilenetv2
+from src.models.resnet18 import load_resnet18
 
 
 def load_cnn_model(backbone, weights_path, input_shape):
-    """
-    Generic CNN loader.
-    Currently supports only MobileNetV2.
-    """
 
     if backbone == "mobilenetv2":
         return load_mobilenetv2(
+            weights_path=weights_path,
+            input_shape=input_shape,
+        )
+
+    elif backbone == "resnet18":
+        return load_resnet18(
             weights_path=weights_path,
             input_shape=input_shape,
         )
