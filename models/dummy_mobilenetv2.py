75012f6b79b583b6ea08ecfd1f0e3d801176806b
PabloJimenez823 <pablojimenezlizcano@gmail.com>
2026-02-05 16:57:50 +0100

    Delete models/dummy_mobilenetv2.py

--- models/dummy_mobilenetv2.py
+++
@@ -1,8 +0,0 @@
-from models.mobilenetv2 import build_mobilenetv2
-
-# Task 1 → 448x448
-model = build_mobilenetv2(input_shape=(448, 448, 3))
-
-model.save("models/dummy/task1_spatial_dummy.keras")
-
-print("✅ Dummy model saved")
